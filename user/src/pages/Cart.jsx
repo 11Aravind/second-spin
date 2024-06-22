94cb0b6e9a3184202afa308fc5a60272cb3b5ba3
11Aravind <aravindsreenandanam11@gmail.com>
2024-06-22 17:59:54 +0530

    cart component was updated

--- user/src/pages/Cart.jsx
+++ user/src/pages/Cart.jsx
@@ -13,19 +13,39 @@ const Cart = () => {
         <h3 className="cart-msg">Your Shopping Cart is Empty </h3>
         <Link to="/"> <button className="keep-shoppingBtn">Keep Shoping</button></Link>
       </div>) : (
-        <div className="row">
-          {
-            items.map((item, index) => {
-              return (
-                <div className="cart-row" key={index}>
-                  <div className="imgContainer"><img src={item[0].image} alt="img" /></div>
-                  <p>{item[0].name}</p>
-                  <Quantitybtn item={item} />
-                </div>
+        <div className="main-cart-container">
+          <div className="left">
+            <div className="row">
+              {
+                items.map((item, index) => {
+                  return (
+                    <div className="cart-row" key={index}>
+                      <div className="imgContainer"><img src={item[0].image} alt="img" /></div>
+                      <div className="desc">{item[0].name}
+                      <Quantitybtn item={item} />
+                      </div>
+                  
+                    </div>
 
-              )
-            })
-          }
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className="right">
+           <div className="right-headding"> PRICE DETAILS</div>
+            <div className="main-cart-container">
+              <div>Price (1 item)</div>
+              <div>  ₹2,999</div>
+            </div>
+            <div className="main-cart-container">
+              <div>Total Amount</div>
+              <div> ₹1,368</div>
+            </div>
+            <div className="main-cart-container">
+           <button>Place Holder</button>
+            </div>
+          </div>
         </div>
       )
       }
