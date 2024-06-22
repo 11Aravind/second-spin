5dd21ae54dd696fbd97d702fa0a77ad5f7d25746
11Aravind <aravindsreenandanam11@gmail.com>
2024-06-22 14:44:30 +0530

    product was displayed correctly

--- user/src/pages/ProductDetails.jsx
+++ user/src/pages/ProductDetails.jsx
@@ -1,28 +1,18 @@
 import { useState } from "react";
 import "./css/productdetails.css"
 import { Link, useParams } from "react-router-dom";
+import products from "./data.json"
 const ProductDetails = () => {
-  const id = useParams()
   const [description,isDescriptionVisible]=useState(true)
   const [warenty,isWarrantyVisible]=useState(false)
-  // console.log(id);
-  const product = [
-    {
-      "_id": "66026c09744acd3776cb5b59",
-      "name": "Lumen® - Custom Headlights",
-      "image": "https://ic.carid.com/lumen/items/custom-headlights_6.jpg",
-      "description": "Custom Headlights by Lumen®. Replace hazy and discolored headlights or give the front of your car or truck a whole new look. Whether you just want to improve your vision or upgrade your vehicle’s appearance, Lumen makes it easy and affordable with these bolt-on headlight assemblies. Don’t drive with dim and dangerous opaque headlights or put up with boring and expensive OEM replacements, when you can get improved illumination and custom style that’s easy to install and easy on your wallet. ",
-      "warranty": "Legally, a vehicle manufacturer cannot void the warranty on a vehicle due to an aftermarket part unless they can prove that the aftermarket part caused or contributed to the failure in the vehicle (per the Magnuson Moss Warranty Act (15 U.S.C. 2302(C)) ",
-      "oldPrice": 399,
-      "newPrice": 359,
-      "status": false,
-      "category_id": "66026b1c7e9a53a020885213",
-      "__v": 0
-    }
-  ];
+  const { id } = useParams();
+  const product = products.filter((item) => item._id === id);
+  console.log(id);
+  console.log(product);
+
   return (
     <div className="spaceing">
-      <div className="productDetails-container">
+       <div className="productDetails-container">
         <div className="left">
           {product && <img src={product[0].image} alt="" />}
         </div>
@@ -41,11 +31,11 @@ const ProductDetails = () => {
               product={product}
               onClick={addProduct}
             /> */}
-            <Link to="/cart">
+             <Link to="/cart">
               <button className="small-btn">ADD TO CART</button>
-            </Link>
+            </Link> 
             {/* {showCart && <Cart callbackShowCart={callbackShowCart} />} */}
-          </div>
+      </div>
           <div className="sub-headding-dropdown" onClick={()=>isDescriptionVisible(!description)}>
             <div >Description</div>
             <div className="icon">{description?<i className="bi bi-chevron-up"></i>:<i className="bi bi-chevron-down"></i>}</div>
@@ -61,8 +51,8 @@ const ProductDetails = () => {
           <div className="aboutProductDescription">
             {warenty && product && product[0].warranty}
           </div>
-        </div>
-      </div>
+        </div> 
+      </div> 
     </div>
   )
 }
--- user/src/pages/Products.jsx
+++ user/src/pages/Products.jsx
@@ -1,63 +1,7 @@
 import "./css/product.css"
 import {Link} from "react-router-dom"
+import products from "./data.json"
 const Products = () => {
-    const products=[
-        {
-            "_id": "660269a57e9a53a02088520d",
-            "name": "Maxi Dry Dog Food - Chicken and Liver (20kg)",
-            "image": "https://ic.carid.com/lumen/items/custom-headlights_6.jpg",
-            "description": "Maxi chicken and liver adult is a complete food for adult dogs with regular activity. It contains all the essential ingredients to satisfy your dog's nutritional needs. It promotes healthy digestion and provides overall wellness to your dog.",
-            "oldPrice": 2900,
-            "newPrice": 2610,
-            "status": false,
-            "category_id": "6602681e705e0506eb614022",
-            "__v": 0
-        },
-        {
-            "_id": "66026a107e9a53a02088520f",
-            "name": "Let's Bite Dry Dog Food for Active Adult",
-            "image": "https://ic.carid.com/vision-x/items/9907611_6.jpg",
-            "description": "The best dry dog food is one that is complete, balanced, tasty, and wholesome. At Petsy, choose from healthy dry dog food brands with the best offers that fit your budget and dog’s health requirements. In addition to stocking top dry dog foods, at Petsy, you can also find the best dry dog foods for Shih Tzus, Labradors, Beagles, German Shepherds, and other breeds. When nutrition is one of the most crucial aspects of a dog’s health and well-being, why compromise?",
-            "oldPrice": 1920,
-            "newPrice": 1728,
-            "status": false,
-            "category_id": "6602681e705e0506eb614022",
-            "__v": 0
-        },
-        {
-            "_id": "66026c09744acd3776cb5b59",
-            "name": "FOFOS Dog Toys - Woodplay Bone",
-            "image": "https://ic.carid.com/lumen/sealed-beam/round-sealed-beam-led-headlights_6.jpg",
-            "description": "Fofos woodplay bone is a dog chew toy that is great for teeth and gums. This toy comes as a set of two toys that can help massage your dog's gums and maintain good oral hygiene. This toy contains real wood and acts as a perfect furniture substitute. bone toys such as this allow your dog to chew, bite, fetch and play while getting the right amount of exercise and also make playtime interactive. This toy is ideal for small-medium dogs (5-20kg) of various breeds such as dachshund, pom, spitz, etc above 1 year of age. This toy is strong but not indestructible and is best suited for pet dogs that have moderate chewing and playing styles. Not recommended for aggressive chewers.",
-            "oldPrice": 399,
-            "newPrice": 359,
-            "status": false,
-            "category_id": "66026b1c7e9a53a020885213",
-            "__v": 0
-        },
-        {
-            "_id": "6602712a744acd3776cb5b67",
-            "name": "Chuckit! Dog Toys - Kick Fetch Ball",
-            "image": "https://ic.carid.com/mbrp/exhaust-systems/mdds927_6.jpg",
-            "description": "Just the glimpse of a Chuckit! toy is enough to excite the 'fetch fanatic dog'. The bright colors signal an adventure with major playtime to come. Chuckit! performance toys are designed to take interACTION to the next level. These dynamic products are not intended to be solo play toys. Built on innovation and ergonomic design, Chuckit! has revolutionized the classic game of fetch. This enhanced experience encourages more fun play. Pioneering play since 1998, the human-animal bond is at the heart of every Chuckit! toy we make. Sharing the exhilaration elevates playtime to a new standard. Our toys are as fun for people as they are for pets, getting owners outdoors and inspiring a healthy, active lifestyle. Whether the dog likes toys that fly, float or tumble, we are proud to bring MORE WAYS TO PLAY! These interactive dog toys are fun for pets and pet parents, allowing pets to chase the Chuckit! kickball as pet parents punt, making them great dog toys for boredom.",
-            "oldPrice": 2750,
-            "newPrice": 2475,
-            "status": false,
-            "category_id": "66026b1c7e9a53a020885213",
-            "__v": 0
-        },
-        {
-            "_id": "66027431f9189158d417dd4a",
-            "name": "Farmina Dry Food - N&D Quinoa Dog Weight Management Adult Med/Maxi (7kg)",
-            "image": "	https://ic.carid.com/walker/items/33220_6.jpg",
-            "description": "​​​​​​​Farmina N&D Quinoa -Weight Management - Lamb & Broccoli Asparagus recipe is a complete food for dogs, which helps in weight management of the pets and further help in curing/preventing obesity. 94% of protein from animal origin, 0% artificial preservatives, 0% grains. Low level of FATS: Fats have the highest energetic density among nutrients. It is necessary to reduce the quantity of this highly caloric nutrient to reduce the total energetic density of the product. A high level of Crude fiber contributes to reducing the energetic density of the product. They increase the satiety feeling and reduce the absorption of caloric nutrients which remain trapped in the fiber matrix. Low Glycemic Index source of QUINOA and pea starch deliver glucose slowly to the blood after digestion. This leads to the pet having enough glucose in the blood for a longer time and do not want to feed more often. The slow release of sugar prevents Diabetes and being Overweight. Complete food for adult dogs. N&D Quinoa Weight Management is a complete dietetic food for dogs, suggested for the reduction of excessive body weight.",
-            "oldPrice": 8500,
-            "newPrice": 7500,
-            "status": false,
-            "category_id": "6602681e705e0506eb614022",
-            "__v": 0
-        },
-    ]
   return (
     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4 g-4" style={{"margin": "40px 33px 0px"}}>
     {products.map((product, index) => (
