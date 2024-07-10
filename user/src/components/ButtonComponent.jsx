074c15c9fe4d99d132bc9cd056f0d81f880be2a7
11Aravind <aravindsreenandanam11@gmail.com>
2024-07-10 09:52:40 +0530

    some component and css was added

--- user/src/components/ButtonComponent.jsx
+++ user/src/components/ButtonComponent.jsx
@@ -1,10 +1,35 @@
-import { CartProvider, useCart } from "react-use-cart";
-const ButtonComponent = () => {
+import { useCart } from "react-use-cart";
+const ButtonComponent = ({
+  classs,
+  product,
+  text,
+  viewProduct,
+  checkOut,
+  onClick,
+  orderConfirmation,
+  disableValue
+}) => {
+  const { addItem } = useCart();
   return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default ButtonComponent
+    <>
+      <button
+        onClick={() => {
+          if (viewProduct || checkOut || orderConfirmation) {
+            if (onClick) onClick();
+          } else {
+            let tmpProduct = JSON.parse(JSON.stringify(product));
+            tmpProduct.id = product._id;
+            tmpProduct.price = product.newPrice;
+            // console.log(tmpProduct);
+            addItem(tmpProduct, 1);
+            if (onClick) onClick();
+          }
+        }}
+        className={classs}
+        disabled={disableValue}>
+        {text}
+      </button>
+    </>
+  );
+};
+export default ButtonComponent;
